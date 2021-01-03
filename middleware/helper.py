ae9117dcefa4aaf0dccbc894c70ef1941cfd4b61
605775776 <605775776@qq.com>
2021-01-03 23:25:13 +0800

    0103re

--- middleware/helper.py
+++ middleware/helper.py
@@ -1,4 +1,5 @@
 import random
+import re
 import yaml
 from common.request_handler import RequestsHandler
 from config.setting import Config
@@ -7,6 +8,7 @@ from jsonpath import jsonpath
 # yaml 读取
 f = open(Config.yaml_config_path, encoding='utf-8')
 yaml_data = yaml.load(f, Loader=yaml.FullLoader)
+
 
 def generate_mobile():
     "随机生成手机号码  1[3,5,8,9]+ 9"
@@ -27,14 +29,25 @@ def login():
                     headers={"X-Lemonban-Media-Type": "lemonban.v2"})
     return res
 
-
+# Context().loan_id 读
 # 上下文
 class Context:
-
     token = ""
     member_id = None
     leave_amount = 0
 
+    @property
+    def loan_id(self):
+        """
+        查询数据库，得到loan_id
+        临时变量保存到Context当中
+        return 返回loan标当中的id值
+        """
+        return
+        pass
+    @property
+    def token(self):
+        pass
 
 def save_token():
     data = login()
@@ -47,41 +60,38 @@ def save_token():
     # return {"token": token, "memeber_id": member_id}
     Context.token = token
     Context.member_id = member_id
+
+    leave_amount = 0
     Context.leave_amount = leave_amount
     # 不需要返回值了
+
+
+def replace_label(target):
+    re_pattern = r"#(.*?)#"
+    while re.findall(re_pattern, target):
+        key = re.search(re_pattern, target).group(1)
+        # value = getattr(CaseKeys, key, '')
+        target = re.sub(re_pattern, str(getattr(Context, key)), target, 1)
+    return target
+
 
 def save_loan_id():
     """
     查询数据库，得到loan_id
-    临时变
-
-
-
-
+    临时变量保存到Context当中
+    return 返回loan标当中的id值
+    """
 
 # if __name__ == '__main__':
 #     data = login()
-    # 麻烦
-    # token = data['data']['token_info']['token']
-    # token_type = data['data']['token_info']['token_type']
+# 麻烦
+# token = data['data']['token_info']['token']
+# token_type = data['data']['token_info']['token_type']
 
 
-    # jsonpath -> 专门用来解析json的路径工具
-    # jsonpath(data, '$..token')[0]
-    # jsonpath(data, '$..token_type')[0]
-    # jsonpath(data, '$..id')[0]
-    # data = save_token()
-    # print(data)
-
-
-
-
-
-
-
-
-
-
-
-
-
+# jsonpath -> 专门用来解析json的路径工具
+# jsonpath(data, '$..token')[0]
+# jsonpath(data, '$..token_type')[0]
+# jsonpath(data, '$..id')[0]
+# data = save_token()
+# print(data)
